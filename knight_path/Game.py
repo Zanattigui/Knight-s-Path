a7f1dd73c68bf471fc47441372bb5a235d0cbc21
Guilherme Zanatti <zanattigui@gmail.com>
2025-10-07 17:44:45 -0300

    feat(menu): creating the menu: background, sound and font

--- knight_path/Game.py
+++ knight_path/Game.py
@@ -1,27 +1,20 @@
 import pygame
 
 from knight_path.Menu import Menu
+from knight_path.const import WIN_HEIGHT, WIN_WIDTH
 
 class Game:
     def __init__(self):
         self.window = None
         pygame.init()
-        self.window = pygame.display.set_mode((800, 600))
+        self.window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 
 
     def run(self):
 
         pygame.display.set_caption("Knight’s Path")
-
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()
-            #         quit()
-
         
